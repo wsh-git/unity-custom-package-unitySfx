53f3cd2cdb6e00a90bbeb49d5fed5c6cf549af34
wsh <1603535741@qq.com>
2023-06-26 15:57:18 +0800

    feat: auto generate SoundPath.cs

--- Editor/SoundConfigEditor.cs
+++ Editor/SoundConfigEditor.cs
@@ -1,15 +1,21 @@
+using System.Text;
+using System.IO;
 using UnityEditor;
+using UnityEngine;
 
 namespace Wsh.Sound {
 
-    public class SfxConfigEditor : UnityEditor.Editor {
+    [CustomEditor(typeof(SoundConfig))]
+    public class SoundConfigEditor : Editor {
         
         private SoundConfig soundConfig;
+        private SerializedProperty generateCSharpPath;
         private SerializedProperty bgmConfigDefine;
         private SerializedProperty sfxConfigDefine;
 
         private void OnEnable() {
-            bgmConfigDefine = serializedObject.FindProperty("BgmConfigDataClass");
+            generateCSharpPath = serializedObject.FindProperty("GenerateCSharpPath");
+            bgmConfigDefine = serializedObject.FindProperty("BgmConfigDefine");
             sfxConfigDefine = serializedObject.FindProperty("SfxConfigDefine");
             soundConfig = (SoundConfig)target;
         }
@@ -17,12 +23,48 @@ namespace Wsh.Sound {
         public override void OnInspectorGUI() {
             serializedObject.Update();
             EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(generateCSharpPath);
             EditorGUILayout.PropertyField(bgmConfigDefine, true);
             EditorGUILayout.PropertyField(sfxConfigDefine, true);
             if(EditorGUI.EndChangeCheck()) {
                 serializedObject.ApplyModifiedProperties();
                 EditorUtility.SetDirty(soundConfig);
             }
+
+            if(GUILayout.Button("Auto Generate (SoundId) CSharp Sprite")) {
+                Generate();
+            }
+        }
+
+        private void Generate() {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("namespace Wsh.Sound {\n\n");
+            stringBuilder.Append("    public class SoundPath {\n");
+            
+            for(int i = 0; i < soundConfig.BgmConfigDefine.Count; i++) {
+                GenerateVar(stringBuilder, soundConfig.BgmConfigDefine[i].bgmName, soundConfig.BgmConfigDefine[i].path);
+            }
+            for(int i = 0; i < soundConfig.SfxConfigDefine.Count; i++) {
+                GenerateVar(stringBuilder, soundConfig.SfxConfigDefine[i].sfxName, soundConfig.SfxConfigDefine[i].path);
+            }
+            stringBuilder.Append("    }\n\n");
+            stringBuilder.Append("}");
+            string path = Path.Combine(Application.dataPath, soundConfig.GenerateCSharpPath, "SoundPath.cs");
+            File.WriteAllText(path, stringBuilder.ToString());
+            AssetDatabase.Refresh();
+        }
+
+        private void GenerateVar(StringBuilder stringBuilder, string varName, string varValue) {
+            stringBuilder.Append("        ");
+            stringBuilder.Append("public const string ");
+            stringBuilder.Append(varName.ToUpper());
+            stringBuilder.Append(" = ");
+            stringBuilder.Append("\"");
+            stringBuilder.Append(varValue);
+            stringBuilder.Append("\"");
+            stringBuilder.Append(";");
+            stringBuilder.Append("\n");
         }
     }
 
--- Runtime/ScriptableObjectClass/SoundConfig.cs
+++ Runtime/ScriptableObjectClass/SoundConfig.cs
@@ -8,6 +8,7 @@ namespace Wsh.Sound {
     public class BgmConfigDataClass {
         public string bgmName;
         public float defaultVolume;
+        public string path;
     }
 
     [Serializable]
@@ -15,11 +16,13 @@ namespace Wsh.Sound {
         public string sfxName;
         public float defaultVolume;
         public float minimumInterval;
+        public string path;
     }
 
     //Assets > Create > 2D > ScriptableObject > SoundConfig 来创建数据对象
     [CreateAssetMenu(fileName = "SoundConfig", menuName = "Custom/ScriptableObject/SoundConfig")]
     public class SoundConfig : ScriptableObject {
+        public string GenerateCSharpPath;
         public List<BgmConfigDataClass> BgmConfigDefine = new List<BgmConfigDataClass>();
         public List<SfxConfigDataClass> SfxConfigDefine = new List<SfxConfigDataClass>();
     }
